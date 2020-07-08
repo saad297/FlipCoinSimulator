3c36f89183e9d92c18c1f2530e5195a74813255a
Saad <skamil60@gmail.com>
2020-07-08 18:57:03 +0530

    Counting Heads And Tails

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,10 +1,21 @@
 echo "Welcome to the flip coin simulator program."
 
-x=$(($RANDOM%2))
+HEADCOUNT=0
+TAILCOUNT=0
 
-if [ $x -eq 0 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+for (( i=1;i<=50;i++ ))
+do
+
+	x=$(($RANDOM%2))
+
+	if [ $x -eq 0 ]
+	then
+		((HEADCOUNT++))
+	else
+		((TAILCOUNT++))
+	fi
+done
+
+echo "Heads appeared $HEADCOUNT times"
+
+echo "Tails appeared $TAILCOUNT times"
